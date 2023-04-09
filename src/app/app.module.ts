84c1b8e77a8dd1f25b71e2d3da660a0de9ed9aa7
Manolo Díaz <mn.diaz.f@gmail.com>
2023-04-08 22:11:08 -0400

    start-development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './components/header/header.component';
-import { CarouselComponent, FooterComponent } from './components';
-import { OfferComponent } from './components/offer/offer.component';
-import { ImgIntermedioComponent } from './components/img-intermedio/img-intermedio.component';
-import { HelpToolbarComponent } from './components/help-toolbar/help-toolbar.component';
-
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    OfferComponent,
-    ImgIntermedioComponent,
-    HelpToolbarComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    CarouselComponent
-
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
